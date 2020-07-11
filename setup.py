1cc6879ebb4832607ada10a28522a756c4ea221b
SilenzedAriG <56854879+SilenzedAriG@users.noreply.github.com>
2020-07-11 20:39:55 +0800

    added image uploading

--- setup.py
+++ setup.py
@@ -40,10 +40,39 @@ def setup_db():
         os.path.dirname(__file__), 'db', 'appSecurity.sqlite')
     db_session.global_init(db_file)
 
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'templates/static/images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/UploadImage', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('aboutus.aboutus'))
 
 def register_blueprints():
     from views import home, shop, account, about
-
     app.register_blueprint(home.blueprint)
     app.register_blueprint(shop.blueprintShop)
     app.register_blueprint(account.blueprintaccounts)
